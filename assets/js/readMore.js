b3b9067a61e31a269c205fd48a8d49a673143f3a
michalskirobert <majointa@gmail.com>
2020-10-28 13:58:27 +0100

    update nov

--- assets/js/readMore.js
+++ assets/js/readMore.js
@@ -1,33 +1,32 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-    var body = document.getElementById("body");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "block";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "block";
-  }
-  }
+const dots = document.querySelectorAll(".dots");
+const moreText = document.querySelectorAll(".more");
+const btnText = document.querySelectorAll(".button__default");
+const jpBtn = document.querySelectorAll("html:lang(ja) .button__default");
 
-  function myFunctionJapanese() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-    var body = document.getElementById("body");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "block";
-      btnText.innerHTML = "さらに読む"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "隠す"; 
-      moreText.style.display = "block";
-  }
-  }
+jpBtn,
+  dots,
+  moreText,
+  btnText.forEach(function (item, index) {
+    btnText[index].addEventListener("click", () => {
+      if (dots[index].style.display === "none") {
+        dots[index].style.display = "block";
+        btnText[index].innerHTML = "Read more";
+        moreText[index].style.display = "none";
+      } else {
+        dots[index].style.display = "none";
+        btnText[index].innerHTML = "Read less";
+        moreText[index].style.display = "block";
+      }
+    });
+    jpBtn[item].addEventListener("click", () => {
+      if (dots[index].style.display === "none") {
+        dots[index].style.display = "block";
+        btnText[index].innerHTML = "さらに読む";
+        moreText[index].style.display = "none";
+      } else {
+        dots[index].style.display = "none";
+        btnText[index].innerHTML = "隠す";
+        moreText[index].style.display = "block";
+      }
+    });
+  });
