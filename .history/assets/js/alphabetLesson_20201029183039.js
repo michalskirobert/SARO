5b99d66ca11ab5f079dc7652baa0fd6a62985ac3
michalskirobert <majointa@gmail.com>
2020-10-29 19:10:42 +0100

    Update
    Fixed all bugs, and create new features

---
+++ .history/assets/js/alphabetLesson_20201029183039.js
@@ -0,0 +1,21 @@
+const audio = document.querySelectorAll(".audio");
+const playBtn = document.querySelectorAll(".sound");
+
+audio,
+  playBtn.forEach((item, play) => {
+    playBtn[play].addEventListener("click", () => {
+      if (audio[play].paused) {
+        audio[play].currentTime = 0;
+        audio[play].play();
+        playBtn[play].src = "./assets/img/icons/speaker--muted.svg";
+      } else {
+        audio[play].pause();
+        playBtn[play].src = "./assets/img/icons/speaker.svg";
+      }
+      audio[play].onended = () => {
+        playBtn[play].src = "./assets/img/icons/speaker.svg";
+      };
+    });
+  });
+
+//Robert Michalski
---
+++ .history/assets/js/alphabetLesson_20201029183144.js
@@ -0,0 +1,21 @@
+const audio = document.querySelectorAll(".audio");
+const playBtn = document.querySelectorAll(".sound");
+
+audio,
+  playBtn.forEach((item, play) => {
+    item.addEventListener("click", () => {
+      if (audio[play].paused) {
+        audio[play].currentTime = 0;
+        audio[play].play();
+        item.src = "./assets/img/icons/speaker--muted.svg";
+      } else {
+        audio[play].pause();
+        item.src = "./assets/img/icons/speaker.svg";
+      }
+      audio[play].onended = () => {
+        item.src = "./assets/img/icons/speaker.svg";
+      };
+    });
+  });
+
+//Robert Michalski
