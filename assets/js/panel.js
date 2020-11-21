72e214e1267f59b004f7f4ba73d938cf5d3090f1
michalskirobert <majointa@gmail.com>
2020-11-21 22:59:15 +0100

    update nov

--- assets/js/panel.js
+++ assets/js/panel.js
@@ -1,78 +1,8 @@
-const acc = document.getElementsByClassName("lesson");
+const acc = document.getElementsByClassName("acc");
 let panel;
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-const btnDialogue = document.getElementsByClassName("dialogue");
-
-for (let i = 0; i < btnDialogue.length; i++) {
-  btnDialogue[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-const buttonMusic = document.getElementsByClassName("buttonMusic");
-
-for (let i = 0; i < buttonMusic.length; i++) {
-  buttonMusic[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-const buttonMovies = document.getElementsByClassName("buttonMovies");
-
-for (let i = 0; i < buttonMovies.length; i++) {
-  buttonMovies[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-const buttonGames = document.getElementsByClassName("buttonGames");
-
-for (let i = 0; i < buttonGames.length; i++) {
-  buttonGames[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-const buttonSeries = document.getElementsByClassName("buttonSeries");
-
-for (let i = 0; i < buttonSeries.length; i++) {
-  buttonSeries[i].addEventListener("click", function () {
     this.classList.toggle("active");
     panel = this.nextElementSibling;
     if (panel.style.display === "block") {
