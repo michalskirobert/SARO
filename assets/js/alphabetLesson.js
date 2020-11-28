910784433b2ab7ce9fe38459e99d0e0e6ff22953
michalskirobert <majointa@gmail.com>
2020-11-28 23:09:48 +0100

    fixed

--- assets/js/alphabetLesson.js
+++ assets/js/alphabetLesson.js
@@ -1,43 +1,31 @@
-const language = document.querySelectorAll("html");
+const language = document.querySelector("html").lang;
 const audio = document.querySelectorAll(".audio");
 const playBtn = document.querySelectorAll(".sound");
+let path = "";
 
-for(let i = 0; i < language.length; i++){
-  if(language[i].lang === "en"){
-audio,
-  playBtn.forEach((item, play) => {
-    item.addEventListener("click", () => {
-      if (audio[play].paused) {
-        audio[play].currentTime = 0;
-        audio[play].play();
-        item.src = "./assets/img/icons/speaker--muted.svg";
-      } else {
-        audio[play].pause();
-        item.src = "./assets/img/icons/speaker.svg";
-      }
-      audio[play].onended = () => {  //ta funkcja nam pozwoli wrócić do poprzedniej ikonki z głośnikiem, moglibyśmy użyć promisy, ale to najlepszy sposób (moim zdaniem) /Robert
-        item.src = "./assets/img/icons/speaker.svg";
-      };
-    });
-  });
-}else {
+(function () {
+  if (language !== "en") {
+    path = "./../";
+  } else {
+    path = "./";
+  }
   audio,
-  playBtn.forEach((item, play) => {
-    item.addEventListener("click", () => {
-      if (audio[play].paused) {
-        audio[play].currentTime = 0;
-        audio[play].play();
-        item.src = "./../assets/img/icons/speaker--muted.svg";
-      } else {
-        audio[play].pause();
-        item.src = "./../assets/img/icons/speaker.svg";
-      }
-      audio[play].onended = () => {
-        item.src = "./../assets/img/icons/speaker.svg";
-      };
+    playBtn.forEach((item, play) => {
+      item.addEventListener("click", () => {
+        if (audio[play].paused) {
+          audio[play].currentTime = 0;
+          audio[play].play();
+          item.src = `${path}assets/img/icons/speaker--muted.svg`;
+        } else {
+          audio[play].pause();
+          item.src = `${path}assets/img/icons/speaker.svg`;
+        }
+        audio[play].onended = () => {
+          //ta funkcja nam pozwoli wrócić do poprzedniej ikonki z głośnikiem.
+          item.src = `${path}assets/img/icons/speaker.svg`;
+        };
+      });
     });
-  });
-}
-}
+})();
 
 //Robert Michalski
